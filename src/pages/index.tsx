11d5a3bd04e4fda9241269e985b0fd6d5f77fbe5
flvSantos15 <flvSantos300@gmail.com>
2023-01-04 18:17:30 -0300

    feat: home estructure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,32 @@
+import Image from "next/image"
+
+import { styled } from "../styles"
+import { HomeContainer, Product } from "../styles/pages/home"
+
+import camiseta1 from '../assets/shirts/1.png'
+import camiseta2 from '../assets/shirts/2.png'
+import camiseta3 from '../assets/shirts/3.png'
+import camiseta4 from '../assets/shirts/4.png'
+
 export default function Home() {
   return (
-    <h1>Hello world</h1>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta2} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta Y</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   )
 }
