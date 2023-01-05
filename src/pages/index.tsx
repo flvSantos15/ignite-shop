0d27eb18dc3c2db9ad187e44db453a813a6d5cd3
flvSantos15 <flvSantos300@gmail.com>
2023-01-04 22:43:06 -0300

    feat: carrousel + stripe integration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
+import { GetServerSideProps } from "next"
 import Image from "next/image"
 
-import { styled } from "../styles"
+import { useKeenSlider } from 'keen-slider/react'
+
 import { HomeContainer, Product } from "../styles/pages/home"
 
 import camiseta1 from '../assets/shirts/1.png'
@@ -8,10 +10,19 @@ import camiseta2 from '../assets/shirts/2.png'
 import camiseta3 from '../assets/shirts/3.png'
 import camiseta4 from '../assets/shirts/4.png'
 
+import 'keen-slider/keen-slider.min.css';
+
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    }
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={camiseta1} alt="" width={520} height={480} />
 
         <footer>
@@ -19,8 +30,24 @@ export default function Home() {
           <span>R$ 79,90</span>
         </footer>
       </Product>
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={camiseta2} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta Y</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} alt="" width={520} height={480} />
+
+        <footer>
+          <strong>Camiseta Y</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={camiseta4} alt="" width={520} height={480} />
 
         <footer>
           <strong>Camiseta Y</strong>
@@ -30,3 +57,5 @@ export default function Home() {
     </HomeContainer>
   )
 }
+
+export const getServerSideProps: GetServerSideProps = () => { }
