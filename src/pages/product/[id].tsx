d7ea30fe987b98b954445a962f3611464920cefd
flvSantos15 <flvSantos300@gmail.com>
2023-01-05 21:58:27 -0300

    feat: estrutura da pagina do produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,10 +1,30 @@
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 
 export default function Product() {
   const router = useRouter()
   const { id } = router.query
 
   return (
-    <h1>Produto encontrado: {id}</h1>
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 65,39</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Aspernatur rerum, fugiat dolor voluptatibus ad dolore unde.
+          Quia ut exercitationem hic ex voluptatem eligendi quis voluptas
+          illum cumque distinctio, earum quasi.
+        </p>
+
+        <button>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
