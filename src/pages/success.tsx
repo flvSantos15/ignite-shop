fabd43ce68f33558e7797b1e7888f4e923622fa2
flvSantos15 <flvSantos300@gmail.com>
2023-01-11 14:31:53 -0300

    feat:success page estructure

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,20 @@
+import Link from 'next/link'
+import { ImageContainer, SuccessContainer } from '../styles/pages/sucess'
+
 export default function Success() {
   return (
-    <h1>Success</h1>
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhull <strong>Flavio Santos</strong>, sua{' '}
+        <strong>Camiseta Beyond the Limits</strong> já está a caminh da sua
+        casa.
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
   )
 }
