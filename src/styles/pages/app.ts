bc6baf06a13bb2a12f4af8605b84ec38501ba41d
flvSantos15 <flvSantos300@gmail.com>
2023-02-01 23:04:55 -0300

    feat: button to cart in header added

--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -5,12 +5,26 @@ export const Container = styled('div', {
   flexDirection: 'column',
   alignItems: 'flex-start',
   justifyContent: 'center',
-  minHeight: '100vh'
+  minHeight: '100vh',
+  paddingBottom: '2rem'
 })
 
 export const Header = styled('header', {
+  display: 'flex',
+  justifyContent: 'space-between',
+
   padding: '2rem 0',
   width: '100%',
   maxWidth: 1180,
-  margin: '0 auto'
+  margin: '0 auto',
+
+  div: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: '0.75rem',
+    gap: '0.75rem',
+
+    backgroundColor: '#202024',
+    borderRadius: '6px'
+  }
 })
